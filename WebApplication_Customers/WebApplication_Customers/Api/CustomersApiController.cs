8d8031d9f8205f07c01cbff1afbbace08e60b124
Taco0508 <taco0508@gmail.com>
2018-04-27 00:48:11 +0800

    完成新增 修改 刪除 功能

--- WebApplication_Customers/WebApplication_Customers/Api/CustomersApiController.cs
+++ WebApplication_Customers/WebApplication_Customers/Api/CustomersApiController.cs
@@ -6,11 +6,14 @@ using System.Net.Http;
 using System.Web.Http;
 
 using WebApplication_Customers.EF;
+using System.Data.Entity;
 
 namespace WebApplication_Customers.Api
 {
     public class CustomersApiController : ApiController
     {
+        NorthwindEntities dc = new NorthwindEntities();
+
         /// <summary>
         /// 查出所有的客戶資料
         /// </summary>
@@ -18,8 +21,69 @@ namespace WebApplication_Customers.Api
         [HttpGet]
         public List<Customers> GetAll()
         {
-            NorthwindEntities dc = new NorthwindEntities();
-            return dc.Customers.ToList();
+            try
+            {
+                return dc.Customers.ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 新增一筆客戶記錄
+        /// </summary>
+        /// <param name="customers"></param>
+        [HttpPost]
+        public void Create(Customers customers)
+        {
+            try
+            {
+                dc.Customers.Add(customers);
+                dc.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 修改一筆客戶記錄
+        /// </summary>
+        /// <param name="customers"></param>
+        [HttpPut]
+        public void Update(Customers customers)
+        {
+            try
+            {
+                dc.Entry(customers).State = System.Data.Entity.EntityState.Modified;
+                dc.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 刪除一筆客戶記錄
+        /// </summary>
+        /// <param name="customers"></param>
+        [HttpDelete]
+        public void Delete(Customers customers)
+        {
+            try
+            {
+                Customers DeleteCustomers = dc.Customers.Find(customers.CustomerID);
+                dc.Customers.Remove(DeleteCustomers);
+                dc.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
